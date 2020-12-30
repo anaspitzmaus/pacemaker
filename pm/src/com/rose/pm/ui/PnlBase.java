8c127beafee6ac2f1f7c355a069c69742b6bcc7c
Ekkehard Rose <rose.ekkehard@t-online.de>
2020-12-31 00:16:04 +0100

    pnlBase

--- pm/src/com/rose/pm/ui/PnlBase.java
+++ pm/src/com/rose/pm/ui/PnlBase.java
@@ -3,14 +3,9 @@ package com.rose.pm.ui;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JScrollPane;
-import net.miginfocom.swing.MigLayout;
+
 import javax.swing.JTable;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JTextField;
-import javax.swing.JComboBox;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
+
 
 public class PnlBase extends JPanel {
 	/**
