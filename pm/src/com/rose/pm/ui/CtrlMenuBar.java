f3f1cc366a29f525f1ebd45a48b2bdd5a229afd6
Ekkehard Rose <rose.ekkehard@t-online.de>
2021-02-07 20:31:27 +0100

    070221

--- pm/src/com/rose/pm/ui/CtrlMenuBar.java
+++ pm/src/com/rose/pm/ui/CtrlMenuBar.java
@@ -4,9 +4,16 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
-import javax.swing.ImageIcon;
+import java.util.HashMap;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import com.rose.Isynet;
 import com.rose.pm.ui.settings.DlgPath;
 
 
@@ -14,6 +21,7 @@ public class CtrlMenuBar {
 	FrmMenuBar menuBar;
 	ManufacturerListener manufacturerListener;
 	SettingsListener settingsListener;
+	UpdateListener updateListener;
 	
 	protected FrmMenuBar getMenuBar() {
 		return this.menuBar;
@@ -41,6 +49,33 @@ public class CtrlMenuBar {
 		menuBar.addManufacturerListener(manufacturerListener);
 		settingsListener = new SettingsListener();
 		menuBar.addSettingsListener(settingsListener);
+		updateListener = new UpdateListener();
+		menuBar.addUpdateListener(updateListener);
+	}
+	
+	/**
+	 * listener for refreshing the active patient as set in isynet
+	 * @author Ekki
+	 *
+	 */
+	class UpdateListener implements ActionListener{
+		Isynet isynet;
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			isynet = new Isynet();
+			
+			try {
+				HashMap<Integer, String> data = isynet.readPatInfo();
+				//create a new Patient out of the data 
+				//put the patients name to the menuBar
+			} catch (FileNotFoundException e1) {					
+				JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Dateipfad" , JOptionPane.ERROR_MESSAGE);
+			} catch (IOException e1) {				
+				JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Dateipfad" , JOptionPane.ERROR_MESSAGE);
+			}
+			
+		}
+		
 	}
 
 
--- pm/src/com/rose/pm/ui/FrmMenuBar.java
+++ pm/src/com/rose/pm/ui/FrmMenuBar.java
@@ -8,6 +8,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JMenuBar;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+
+import com.rose.pm.ui.CtrlMenuBar.UpdateListener;
+import java.awt.Color;
 
 public class FrmMenuBar extends JMenuBar{
 	JButton btnManufacturer;
@@ -21,20 +26,33 @@ public class FrmMenuBar extends JMenuBar{
 	private static final long serialVersionUID = 4199586390816416921L;
 	
 	public FrmMenuBar() {
+		setBackground(Color.ORANGE);
 		font = new Font("Tahoma", Font.PLAIN, 14);
+		
 		btnManufacturer = new JButton("btnManufacturer");
+		btnManufacturer.setBackground(new Color(0, 206, 209));
 		btnManufacturer.setFont(font);
 		add(btnManufacturer);
+		
+		JLabel lblSep = new JLabel("  |  ");
+		Font fontSep = new Font("Tahoma", Font.PLAIN, 20);
+		//fontSep = fontSep.deriveFont((float) 10);
+		lblSep.setFont(fontSep);
+		add(lblSep);
+		
 		
 		lblPatient = new JLabel("lblPatient");
 		lblPatient.setFont(font);
 		add(lblPatient);
 		
 		btnUpdatePatient = new JButton();
+		btnUpdatePatient.setBackground(new Color(0, 206, 209));
 		btnUpdatePatient.setFont(font);
 		add(btnUpdatePatient);
 		
 		btnSettings = new JButton("btnSettings");
+		btnSettings.setForeground(new Color(0, 0, 0));
+		btnSettings.setBackground(new Color(0, 206, 209));
 		btnSettings.setFont(font);
 		add(Box.createHorizontalGlue());  //horizontal glue
 		add(btnSettings);
@@ -58,13 +76,11 @@ public class FrmMenuBar extends JMenuBar{
 	}
 
 	protected void addSettingsListener(ActionListener settingsListener) {
-		btnSettings.addActionListener(settingsListener);
-		
+		btnSettings.addActionListener(settingsListener);		
 	}
 
 	protected void setBtnSettingsText(String txt) {
-		btnSettings.setText(txt);
-		
+		btnSettings.setText(txt);		
 	}
 
 	protected void setBtnUpdateIcon(ImageIcon icon) {
@@ -73,6 +89,10 @@ public class FrmMenuBar extends JMenuBar{
 	
 	protected void setBtnUpdateText(String txt) {
 		btnUpdatePatient.setText(txt);
+	}
+
+	protected void addUpdateListener(ActionListener listener) {
+		btnUpdatePatient.addActionListener(listener);		
 	}
 
 	
