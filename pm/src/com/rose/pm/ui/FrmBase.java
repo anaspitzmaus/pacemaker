90a58fea0aef0ea726a9b6f76e339b359c08e646
Ekkehard Rose <rose.ekkehard@t-online.de>
2021-02-07 21:31:55 +0100

    070221

--- pm/src/com/rose/pm/ui/FrmBase.java
+++ pm/src/com/rose/pm/ui/FrmBase.java
@@ -8,6 +8,15 @@ import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeListener;
+import javax.swing.JToolBar;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JSeparator;
+import java.awt.Color;
+import javax.swing.SwingConstants;
 
 
 public class FrmBase extends JFrame {
@@ -20,6 +29,14 @@ public class FrmBase extends JFrame {
 	
 	JTabbedPane tabbedPane;
 	JPanel pnlStatistics;
+	private JToolBar toolBar;
+	private JLabel lblIsynet;
+	private JTextField txtPatient;
+	private JLabel lblPatient;
+	private JButton btnRefresh;
+	private Component horizontalStrut;
+	private JSeparator separator;
+	private Component horizontalStrut_1;
 	
 
 	
@@ -54,6 +71,40 @@ public class FrmBase extends JFrame {
 		
 		pnlStatistics = new JPanel();
 		contentPane.add(pnlStatistics, BorderLayout.SOUTH);
+		
+		toolBar = new JToolBar();
+		contentPane.add(toolBar, BorderLayout.NORTH);
+		
+		lblIsynet = new JLabel("lblIsynet");
+		lblIsynet.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		toolBar.add(lblIsynet);
+		
+		horizontalStrut = Box.createHorizontalStrut(20);
+		toolBar.add(horizontalStrut);
+		
+		separator = new JSeparator();
+		separator.setOrientation(SwingConstants.VERTICAL);
+		separator.setBackground(new Color(0, 128, 0));
+		separator.setForeground(new Color(0, 0, 0));
+		toolBar.add(separator);
+		
+		horizontalStrut_1 = Box.createHorizontalStrut(20);
+		toolBar.add(horizontalStrut_1);
+		
+		lblPatient = new JLabel("lblPatient");
+		lblPatient.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		toolBar.add(lblPatient);
+		
+		txtPatient = new JTextField();
+		txtPatient.setHorizontalAlignment(SwingConstants.LEFT);
+		txtPatient.setEditable(false);
+		txtPatient.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		toolBar.add(txtPatient);
+		txtPatient.setColumns(10);
+		
+		btnRefresh = new JButton("btnRefresh");
+		btnRefresh.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		toolBar.add(btnRefresh);
 	}
 	
 	protected void addTabChangeListener(ChangeListener l) {
