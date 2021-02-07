aa70aff5f6fd318771082d290777d561805a1886
user2 <user2@CLIENT2.KARDIO.local>
2021-02-07 10:09:49 +0100

    070221

--- pm/src/com/rose/Isynet.java
+++ pm/src/com/rose/Isynet.java
@@ -4,10 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.prefs.Preferences;
 
 import com.rose.person.Patient;
@@ -25,20 +24,22 @@ public class Isynet {
 		
 	}
 	
-	public void readPatInfo() {
+	public HashMap<Integer, String> readPatInfo() {
 		BufferedReader reader;
-		
+		HashMap<Integer, String> data = new HashMap<Integer, String>();
 		
 		prefs = Preferences.userNodeForPackage(CtrlSetPathPatInfo.class);
 		String patInfoPath = prefs.get("Path_PatInfo", "");
 		path = Paths.get( patInfoPath);
 		if(patInfoPath != "") {
 			try {
-				File file = new File(patInfoPath);
+				File file = new File(patInfoPath, "Patinf02.tmp");
 				reader = new BufferedReader(new FileReader(file));
 				String line = reader.readLine();
 				while(line!= null) {
-					System.out.println(line);
+					String k = line.substring(0, 4);
+					String v = line.substring(5);
+					data.put(Integer.parseInt(k), v);
 					line = reader.readLine();
 				}
 				reader.close();
@@ -47,6 +48,7 @@ public class Isynet {
 				e.printStackTrace();
 			}			
 		}
+		return data;
 	}
 	
 	
