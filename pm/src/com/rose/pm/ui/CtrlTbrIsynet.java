40da4b28a710ed8704c9e1d7f9f1d0d0fcc483de
user2 <user2@CLIENT2.KARDIO.local>
2021-02-10 13:48:03 +0100

    100221

--- pm/src/com/rose/pm/ui/CtrlTbrIsynet.java
+++ pm/src/com/rose/pm/ui/CtrlTbrIsynet.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.HashMap;
 
 import javax.swing.ImageIcon;
@@ -22,6 +23,7 @@ public class CtrlTbrIsynet {
 	Isynet isynet;
 	HashMap<Integer, String> patData;
 	Patient patient;
+	UpdateListener updateListener;
 	
 	protected TbrIsynet getToolBar() {
 		return tbrIsynet;
@@ -31,6 +33,7 @@ public class CtrlTbrIsynet {
 	public CtrlTbrIsynet() {
 		isynet = new Isynet();
 		tbrIsynet = new TbrIsynet();
+		setListener();		
 		readPatInfo();
 		if(patData != null) {
 			createPatient();
@@ -43,6 +46,15 @@ public class CtrlTbrIsynet {
 		iconRefresh.getImage();
 		tbrIsynet.setBtnRefreshIcon(iconRefresh);
 		tbrIsynet.setBtnRefreshText("");
+		tbrIsynet.setLblPatientTxt("Patient:");
+		tbrIsynet.setLblIsynetText("Isynet");
+		tbrIsynet.setLblPatBirthText("geb. am:");
+		tbrIsynet.setLblPatNrText("Patientennummer:");
+	}
+	
+	private void setListener() {
+		updateListener = new UpdateListener();
+		tbrIsynet.addUpdateListener(updateListener);
 	}
 	
 	private void readPatInfo(){
@@ -69,7 +81,7 @@ public class CtrlTbrIsynet {
 		String lastname, firstname;
 		
 		if(!patData.isEmpty()) {
-			
+			//surname and firstname
 			String patName = patData.get(3301);
 			String[] val= patName.split(",");
 			if(val.length == 2) {
@@ -77,6 +89,21 @@ public class CtrlTbrIsynet {
 				firstname = val[1];
 				patient = new Patient(lastname, firstname);
 			}
+			//birthday
+			String birth = patData.get(3303);
+			
+			Integer day = Integer.valueOf(birth.substring(0, 2));
+			Integer month = Integer.valueOf(birth.substring(2, 4));
+			Integer year = Integer.valueOf(birth.substring(4, 6));
+			year = 1900 + year;
+			
+			LocalDate birthday = LocalDate.of(year, month, day);
+			
+			patient.setBirthday(birthday);
+			
+			//number of patient
+			Integer nr = Integer.parseInt(patData.get(3600));
+			patient.setNumber(nr);
 		}
 	}
 	
@@ -85,6 +112,7 @@ public class CtrlTbrIsynet {
 	 */
 	protected void insertPatient() {
 		tbrIsynet.setPatientData(patient);
+		
 	}
 	
 	public static Image getImage(final String pathAndFileName) {
@@ -101,18 +129,13 @@ public class CtrlTbrIsynet {
 		Isynet isynet;
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			isynet = new Isynet();
-			
-			try {
-				HashMap<Integer, String> data = isynet.readPatInfo();
-				//create a new Patient out of the data 
-				//put the patients name to the menuBar
-			} catch (FileNotFoundException e1) {					
-				JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Dateipfad" , JOptionPane.ERROR_MESSAGE);
-			} catch (IOException e1) {				
-				JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Dateipfad" , JOptionPane.ERROR_MESSAGE);
+			readPatInfo();
+			if(patData != null) {
+				createPatient();
+				if(patient instanceof Patient) {
+					insertPatient();
+				}
 			}
-			
 		}
 		
 	}
--- pm/src/com/rose/pm/ui/TbrIsynet.java
+++ pm/src/com/rose/pm/ui/TbrIsynet.java
@@ -3,7 +3,10 @@ package com.rose.pm.ui;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -26,8 +29,13 @@ public class TbrIsynet extends JToolBar{
 	private Component horizontalStrut;
 	private JSeparator separator;
 	private Component horizontalStrut_1;
+	private JLabel lblPatBirth;
+	private JTextField txtPatBirth;
+	private JLabel lblPatNr;
+	private JTextField txtPatNr;
 	
 	public TbrIsynet() {
+		setBorder(BorderFactory.createLineBorder(Color.BLUE));
 		lblIsynet = new JLabel("lblIsynet");
 		lblIsynet.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		add(lblIsynet);
@@ -55,6 +63,26 @@ public class TbrIsynet extends JToolBar{
 		add(txtPatient);
 		txtPatient.setColumns(10);
 		
+		lblPatBirth = new JLabel("lblPatBirth");
+		lblPatBirth.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(lblPatBirth);
+		
+		txtPatBirth = new JTextField();
+		txtPatBirth.setEditable(false);
+		txtPatBirth.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(txtPatBirth);
+		txtPatBirth.setColumns(10);
+		
+		lblPatNr = new JLabel("lblPatNr");
+		lblPatNr.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(lblPatNr);
+		
+		txtPatNr = new JTextField();
+		txtPatNr.setEditable(false);
+		txtPatNr.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(txtPatNr);
+		txtPatNr.setColumns(10);
+		
 		btnRefresh = new JButton("btnRefresh");
 		btnRefresh.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		add(btnRefresh);
@@ -62,7 +90,8 @@ public class TbrIsynet extends JToolBar{
 
 	public void setPatientData(Patient patient) {
 		txtPatient.setText(patient.getSurname() + ", " + patient.getFirstname());
-		
+		txtPatBirth.setText(patient.getBirthday().getDayOfMonth() + "." + patient.getBirthday().getMonthValue() + "." + patient.getBirthday().getYear());
+		txtPatNr.setText(String.valueOf(patient.getNumber()));
 	}
 
 	protected void setBtnRefreshIcon(ImageIcon iconRefresh) {
@@ -72,6 +101,39 @@ public class TbrIsynet extends JToolBar{
 	protected void setBtnRefreshText(String txt) {
 		btnRefresh.setText(txt);		
 	}
+
+	protected void addUpdateListener(ActionListener listener) {
+		btnRefresh.addActionListener(listener);		
+	}
+
+	protected void setLblPatientTxt(String txt) {
+		lblPatient.setText(txt);		
+	}
 	
+	protected void setLblIsynetText(String txt) {
+		lblIsynet.setText(txt);
+	}
 	
+	protected void setLblPatBirthText(String txt) {
+		lblPatBirth.setText(txt);	
+	}
+	
+	protected void setTxtPatBirth(LocalDate date) {
+		if(date instanceof LocalDate) {
+			txtPatBirth.setText(date.getDayOfMonth() + "." + date.getMonth() + "." + date.getYear());
+		}else{
+			txtPatBirth.setText("");
+		}
+	}
+	
+	protected void setLblPatNrText(String txt) {
+		lblPatNr.setText(txt);
+	}
+	
+	protected void setTxtPatientNr(Integer nr) {
+		txtPatNr.setText(String.valueOf(nr));
+	}
 }
+
+	
+	
